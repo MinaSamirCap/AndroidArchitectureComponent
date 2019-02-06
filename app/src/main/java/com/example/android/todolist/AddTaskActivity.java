c7d5a260c0d916d96af0f633d34e8624fe9fd7f1
MinaSamir <menasamercap@gmail.com>
2019-02-06 20:01:53 +0200

    added liveData while update the task

--- app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -16,8 +16,11 @@
 
 package com.example.android.todolist;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -70,17 +73,12 @@ public class AddTaskActivity extends AppCompatActivity {
             if (mTaskId == DEFAULT_TASK_ID) {
                 // populate the UI
                 mTaskId = intent.getIntExtra(EXTRA_TASK_ID, DEFAULT_TASK_ID);
-                AppExecutor.getsInstance().diskIO().execute(new Runnable() {
+                final LiveData<TaskEntry> task = mDb.taskDao().loadTaskById(mTaskId);
+                task.observe(this, new Observer<TaskEntry>() {
                     @Override
-                    public void run() {
-                        final TaskEntry taskEntry = mDb.taskDao().loadTaskById(mTaskId);
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                pupulateUi(taskEntry);
-                            }
-                        });
-
+                    public void onChanged(@Nullable TaskEntry taskEntry) {
+                        task.removeObserver(this);
+                        pupulateUi(taskEntry);
                     }
                 });
             }
@@ -88,7 +86,7 @@ public class AddTaskActivity extends AppCompatActivity {
     }
 
     private void pupulateUi(TaskEntry taskEntry) {
-        if(taskEntry == null) return;
+        if (taskEntry == null) return;
         mEditText.setText(taskEntry.getDescription());
         setPriorityInViews(taskEntry.getPriority());
 
@@ -137,9 +135,9 @@ public class AddTaskActivity extends AppCompatActivity {
         AppExecutor.getsInstance().diskIO().execute(new Runnable() {
             @Override
             public void run() {
-                if(mTaskId == DEFAULT_TASK_ID){
+                if (mTaskId == DEFAULT_TASK_ID) {
                     mDb.taskDao().insertTask(taskEntry);
-                }else {
+                } else {
                     taskEntry.setId(mTaskId);
                     mDb.taskDao().updateTask(taskEntry);
                 }
